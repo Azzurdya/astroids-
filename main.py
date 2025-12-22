442b8e8d625e76c864c0720c10e044f8ae27c7f6
Azzurdya <suriraaditya08@gmail.com>
2025-12-22 21:22:08 +1300

    added a lot

--- main.py
+++ main.py
@@ -4,22 +4,48 @@ import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 Game = TRUE
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+dt = 0
+fps = pygame.time.Clock()
+x = SCREEN_WIDTH / 2
+y = SCREEN_HEIGHT / 2
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+Player.containers = (updatable, drawable)
+player_ship = Player(x, y, 10)
 
 
 def main():
     print("Starting Asteroids with pygame version: 2.6.1")
     print("Screen width: 1280")
     print("Screen height: 720")
+
     pygame.init()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+    fps = pygame.time.Clock()
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player_ship = Player(x, y, 10)
+
     while Game == True:
         log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        dt = fps.tick(60) / 1000
         screen.fill("black")
+        updatable.update(dt)
+        for object in drawable:
+            object.draw(screen)
+        fps.tick(60)
         pygame.display.flip()
 
 
